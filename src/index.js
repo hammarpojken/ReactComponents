b1c1d3177e5840e2524f8c108cd9e4f2c8dd70d2
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-17 13:59:03 +0100

    Add a comment Component to App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail.js";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        name={faker.name.firstName()}
+        avatar={faker.image.avatar()}
+        text={faker.lorem.text()}
+        date={faker.date.month()}
+      />
+      <CommentDetail
+        name={faker.name.firstName()}
+        avatar={faker.image.avatar()}
+        text={faker.lorem.text()}
+        date={faker.date.month()}
+      />
+      <CommentDetail
+        name={faker.name.firstName()}
+        avatar={faker.image.avatar()}
+        text={faker.lorem.text()}
+        date={faker.date.month()}
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
