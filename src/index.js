7283c253f920cceb9d8091e77e0be5b8775a2275
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-17 14:50:51 +0100

    Add ApprovalCard component to wrap the comments

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail.js";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        name={faker.name.firstName()}
-        avatar={faker.image.avatar()}
-        text={faker.lorem.text()}
-        date={faker.date.month()}
-      />
-      <CommentDetail
-        name={faker.name.firstName()}
-        avatar={faker.image.avatar()}
-        text={faker.lorem.text()}
-        date={faker.date.month()}
-      />
-      <CommentDetail
-        name={faker.name.firstName()}
-        avatar={faker.image.avatar()}
-        text={faker.lorem.text()}
-        date={faker.date.month()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          name={faker.name.firstName()}
+          avatar={faker.image.avatar()}
+          text={faker.lorem.sentence()}
+          date={faker.date.month()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          name={faker.name.firstName()}
+          avatar={faker.image.avatar()}
+          text={faker.lorem.sentence()}
+          date={faker.date.month()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          name={faker.name.firstName()}
+          avatar={faker.image.avatar()}
+          text={faker.lorem.sentence()}
+          date={faker.date.month()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
